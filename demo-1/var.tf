b5f867eca95e6dbffbb92ad2f6b2f0719e1b6305
Rajiv Gupta <rajiv.gupta2@globallogic.com>
2022-07-13 16:17:11 +0530

    demo-1

--- demo-1/var.tf
+++ demo-1/var.tf
@@ -15,7 +15,7 @@ variable "aws_region" {
 variable "amis" {
     type = map(string)
     default = {
-        us-east-1 = "ami-13be557e"
+        us-east-1 = "ami-0cff7528ff583bf9a"
         //us-west-2 = "ami-123"
         //eu-west-1 = "ami-xyz"
     }
