6b239f427fded86f8d839ce0ff5ed4391a428f94
rajiv467 <80842893+rajiv467@users.noreply.github.com>
2022-09-19 13:12:29 +0530

    Update var.tf

--- demo-1/var.tf
+++ demo-1/var.tf
@@ -15,7 +15,7 @@ variable "aws_region" {
 variable "amis" {
     type = map(string)
     default = {
-        us-east-1 = "ami-0cff7528ff583bf9a"
+        us-east-1 = "ami-05fa00d4c63e32376"
         //us-west-2 = "ami-123"
         //eu-west-1 = "ami-xyz"
     }
